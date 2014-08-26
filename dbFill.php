2cf782cc04a64c73653874f12857e4991e2c8763
teknogeek <jmargolis97@gmail.com>
2014-08-26 11:19:40 -0400

    work on address indexing

--- dbFill.php
+++ dbFill.php
@@ -3,11 +3,34 @@
 require_once('easybitcoin.php');
 $tesla = new Bitcoin('tesla', 'swag', 'localhost', '1857');
 
-$tx = $tesla->gettransaction('6a668098447c9f4121533ed96dbe390efe4329b708d0bce8f58efbcd31aed2d0');
+$credFile = file_get_contents('mysql.properties');
+$credRows = explode("\r\n", $credFile);
+foreach($credRows as $value)
+{
+    $valueSplit = explode("::", $value);
+    $creds[$valueSplit[0]] = $valueSplit[1];
+}
 
-echo '<pre>';
-echo print_r($tx, 1);
-echo '</pre>';
-//$con = mysqli_connect('localhost', 'root', '', 'tesla');
+$db = new PDO($creds["serverInfo"], $creds["username"], $creds["password"]);
+
+for($i = 0; $i < $tesla->getblockcount(); $i++)
+{
+    $hash = $tesla->getblockhash($i);
+    $blockData = $tesla->getblock($hash, true);
+
+    foreach($blockData['tx'] as $tx)
+    {
+        foreach($tx['vout'] as $out)
+        {
+            if(array_key_exists('addresses', $out['scriptPubKey']))
+            {
+                $db->exec("INSERT INTO `addresses` (address, balance) VALUES ('".$out['scriptPubKey']['addresses'][0]."', '".$out['value']."') ON DUPLICATE KEY UPDATE balance = balance + " . $out['value']);
+            }
+        }
+    }
+    //$stmt = $db->prepare("SELECT * FROM table WHERE id=:id AND name=:name");
+    //$stmt->execute(array(':id' => $id, ':name' => $name));
+    //$testRows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
 </html>
